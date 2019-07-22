045cd1997dbd356f6488b7e39fd280fe6417647a
Antoine Leroyer <aleroyer@deezer.com>
2019-07-22 10:22:06 +0200

    Add alt gr to typematrix mapping

--- keyboards/xd75/keymaps/typematrix/keymap.c
+++ keyboards/xd75/keymaps/typematrix/keymap.c
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |        +--------+--------+--------+--------+--------+        +--------+--------+--------+--------+--------------------------+        |
  * |        | Z      | X      | C      | V      | B      |        | N      | M      | ,      | .      | /      | \      | UP     |        |
  * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+--------+-----------------+--------+--------|
- * | CTRL   | ALT    | CMD    | FN     | SPACE  | SPACE  |        | SPACE  | SPACE  |        | HOME   | END    | LEFT   | DOWN   | RIGHT  |
+ * | CTRL   | ALT    | CMD    | FN     | SPACE  | SPACE  |        | SPACE  | SPACE  | ALGR   | HOME   | END    | LEFT   | DOWN   | RIGHT  |
  * '--------------------------------------------------------------------------------------------------------------------------------------'
  */
 
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         {KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_BSPACE, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_PGDOWN},
         {KC_LSFT, KC_A, KC_S, KC_D, KC_F, KC_G, KC_ENTER, KC_H, KC_J, KC_K, KC_L, KC_SCOLON, KC_QUOTE, KC_GRV, KC_RSHIFT},
         {_______, KC_Z, KC_X, KC_C, KC_V, KC_B, _______, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_BSLASH, KC_UP, _______},
-        {KC_LCTRL, KC_LALT, KC_LGUI, MO(_FN), KC_SPC, KC_SPC, _______, KC_SPC, KC_SPC, _______, KC_HOME, KC_END, KC_LEFT, KC_DOWN, KC_RIGHT},
+        {KC_LCTRL, KC_LALT, KC_LGUI, MO(_FN), KC_SPC, KC_SPC, _______, KC_SPC, KC_SPC, KC_ALGR, KC_HOME, KC_END, KC_LEFT, KC_DOWN, KC_RIGHT},
     },
 
     /* FUNCTION
